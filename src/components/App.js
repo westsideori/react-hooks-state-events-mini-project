b3053f729c9b77bbff9ccdf71af0956fd38b4ddd
Ori Markowitz <ori.markowitz@gmail.com>
2021-02-03 18:22:22 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,14 +8,22 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [ selected, setSelected ] = useState("All");
+  const [ tasks, setTasks ] = useState(TASKS)
+  const filteredTasks = (selected === "All" ? tasks : tasks.filter(task => task.category === selected))
+  function onTaskFormSubmit(newTask){
+    setTasks([...tasks, newTask])
+  }
+  
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selected={selected} setSelected={setSelected}/>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={filteredTasks} selected={selected}/>
     </div>
   );
 }
 
 export default App;
+
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, setSelected, selected}) {
+
+  const catList = categories.map((cat, index) => {
+    // let classN = (cat === selected ? "selected" : null)
+    return <button key={index} onClick={() => setSelected(cat)} className={cat === selected ? "selected" : null}>{cat}</button >
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {catList}
     </div>
   );
 }
 
 export default CategoryFilter;
+
